b2a7004b68935dfeea36439083d9361b019e9a00
Anastasiia Parsaeva <anastasiia_parsaeva@epam.com>
2018-12-26 19:18:25 +0400

    commit to fix git errors

--- js_basics_task/part B/9. replaceQuotes.js
+++ js_basics_task/part B/9. replaceQuotes.js
@@ -8,9 +8,14 @@
 I’m the “hero” 
 */
 
+function replaceQuotes(inputStr) {
+   // inputStr.replace(regexp|substr, newSubStr|function[, flags])
+    inputStr.replace(/'(?![А-ЯЁ][а-яё])/, `”`);
 
-function replaceQuotes(inputString) {
-    return inputString.replace(/'(\w+)'/g, '"$1"');
+/*
+    Сопоставлется с x, только если за x не следует y. Например, шаблон /\d+(?!\.)/ сопоставляется с числом только если за ним не следует десятичная запятая.
+
+Выражение /\d+(?!\.)/.exec('3.141') сопоставится с «141» но не с «3.141».*/
 }
 
 console.log(replaceQuotes `I’m the ‘hero’`);
