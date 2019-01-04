a4c6c730bb223ac3e3a4c7d47846df29d4145fea
nastya <nast.ovc@yandex.ru>
2019-01-04 13:01:19 +0400

    examples added

--- js_basics_task/part C/basicMathematicalOperations.js
+++ js_basics_task/part C/basicMathematicalOperations.js
@@ -26,3 +26,8 @@ function basicOp(operation, value1, value2)
       return null;
   }
 }
+
+console.log(basicOp('+', 4, 7));
+console.log(basicOp('-', 15, 18));
+console.log(basicOp('*', 5, 5));
+console.log(basicOp('/', 49, 7));
