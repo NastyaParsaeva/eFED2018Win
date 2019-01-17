d2d70f07c22829258dbd026ddd56421abd315b28
nastya <nast.ovc@yandex.ru>
2019-01-17 19:49:52 +0400

    issues from previous pr fixed

--- js/five-days.js
+++ js/five-days.js
@@ -1,14 +1,16 @@
 
-let slides = document.getElementsByClassName('day-weather');
+const slides = document.getElementsByClassName('day-weather');
+const slideNav = document.querySelectorAll('.day-switcher .menu-link');
+const next = document.getElementById('next-day');
+const prev = document.getElementById('prev-day');
 let current = 0;
-let slideNav = document.querySelectorAll('.day-switcher .menu-link');
 
 function changeSlide(index) {
-    slides[current].className = 'day-weather';
-    slideNav[current].className = 'menu-link';
+    slides[current].classList.remove('shown');
+    slideNav[current].classList.remove('selected');
     current = index;
-    slides[current].className = 'day-weather shown';
-    slideNav[current].className = 'menu-link selected';
+    slides[current].classList.add('shown');
+    slideNav[current].classList.add('selected');
 }
  
 function nextSlide() {
@@ -19,12 +21,8 @@ function previousSlide() {
   (current !== 0) ? changeSlide(current - 1) : changeSlide(slides.length - 1);
 }
 
-let next = document.getElementById('next-day');
 next.addEventListener('click', nextSlide);
-
-let prev = document.getElementById('prev-day');
 prev.addEventListener('click', previousSlide);
-
 for (let i = 0; i < slideNav.length; i++) {
   slideNav[i].addEventListener('click', () => {
     changeSlide(i);
--- js/today-page.js
+++ js/today-page.js
@@ -1,13 +1,13 @@
-let graphs = document.querySelectorAll(".graph");
-let buttons = document.querySelectorAll('.graph-controller .menu-link');
-current = 0;
+const graphs = document.querySelectorAll(".graph");
+const buttons = document.querySelectorAll('.graph-controller .menu-link');
+let current = 0;
 
 function changeGraph(index) {
-    graphs[current].className = graphs[current].className.substring(0, graphs[current].className.length - ' shown'.length);
-    buttons[current].className = buttons[current].className.substring(0, buttons[current].className.length - ' selected'.length);
+    graphs[current].classList.remove('shown');
+    buttons[current].classList.remove('selected');
     current = index;
-    graphs[current].className += ' shown';
-    buttons[current].className += ' selected';
+    graphs[current].classList.add('shown');
+    buttons[current].classList.add('selected');
 }
 
 for (let i = 0; i < buttons.length; i++) {
