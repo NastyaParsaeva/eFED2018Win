fa32f9ad50c5438e3a1369d02095e4bcffbbaf69
nastya <nast.ovc@yandex.ru>
2019-01-23 00:30:45 +0400

    five-days-calls added

--- js/ajax_call.js
+++ js/ajax_call.js
@@ -1,6 +1,8 @@
 const APP_ID = 'f9a8867923bcc86f488c9142432dd2de';
 const WEATHER_DETAILS_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&APPID=${ APP_ID }&q=`;
+//const AIR_POLLUTION_ENDPOINT = `http://api.openweathermap.org/pollution/v1/co/${ coords }/current.json?appid=${ APP_ID}`;
 const defaultCity = 'izhevsk';
+let coords;
 const page = {
     init: function() {
         this.getWeatherDetails(defaultCity, this.render);
@@ -23,24 +25,23 @@ const page = {
         xhr.open('GET', url, true);
         xhr.send();
     },
-    render(data) {   
-        
+    render(data) { 
         document.getElementById('chosen-location').innerHTML = `${data.name}, ${data.sys.country}`;
         document.getElementById('today-weekday').innerHTML = new Date(data.dt * 1000).toLocaleString('ru-RU', {weekday: "long"});
         document.getElementById('weather-description').innerHTML = data.weather[0].description;
-        document.getElementById('weather-icon').setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
-        document.getElementById('current-temperature').innerHTML = Math.round(data.main.temp);
-        document.getElementById('today-humidity').innerHTML = data.main.humidity;
-        document.getElementById('today-wind-speed').innerHTML = data.wind.speed;
+        setAttributes(document.getElementById('weather-icon'), {'src' : `http://openweathermap.org/img/w/${data.weather[0].icon}.png`, 'alt' : data.weather[0].description});
+        document.getElementById('current-temperature').innerHTML = `${Math.round(data.main.temp)} °C`;
+        document.getElementById('today-humidity').innerHTML = `Влажность: ${data.main.humidity} %`;
+        document.getElementById('today-wind-speed').innerHTML = `Ветер: ${data.wind.speed.toFixed(1)} м/с`;
         if (data.rain) {
-            data.rain['1h'] ? document.getElementById('today-precipitation').innerHTML = data.rain['1h'] : 
-                              document.getElementById('today-precipitation').innerHTML = data.rain['3h'] ;
+            data.rain['1h'] ? document.getElementById('today-precipitation').innerHTML = `Осадки: ${data.rain['1h']} мм` : 
+                              document.getElementById('today-precipitation').innerHTML = `Осадки: ${data.rain['3h']} мм` ;
         } else {
             if (data.snow) {
-                data.snow['1h'] ? document.getElementById('today-precipitation').innerHTML = data.snow['1h'] : 
-                                  document.getElementById('today-precipitation').innerHTML = data.snow['3h'];
+                data.snow['1h'] ? document.getElementById('today-precipitation').innerHTML = `Осадки: ${data.snow['1h']} мм` : 
+                                  document.getElementById('today-precipitation').innerHTML = `Осадки: ${data.snow['3h']} мм`;
             } else {
-                document.getElementById('today-precipitation').innerHTML = 0;
+                document.getElementById('today-precipitation').innerHTML = `Осадки: 0 мм`;
             }
         }
         // DOM manipulation 
@@ -48,3 +49,9 @@ const page = {
 };
 
 page.init();
+
+function setAttributes(element, attributes) {
+    for(var key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
---
+++ js/five-days-api-calls.js
@@ -0,0 +1,82 @@
+const APP_ID = 'cbb3210df49fdf1c3c675a785e42454b';
+const WEATHER_DETAILS_ENDPOINT = `http://api.openweathermap.org/data/2.5/forecast?units=metric&lang=ru&APPID=${ APP_ID }&q=`;
+const defaultCity = 'izhevsk';
+const page = {
+    init: function() {
+        this.getWeatherDetails(defaultCity, this.render);
+
+        const searchField = document.getElementById('search-field');
+        searchField.addEventListener('change', (event) => {
+            const city = event.target.value;
+            console.log(this);
+            this.getWeatherDetails(city, this.render);
+        });
+    },
+    getWeatherDetails(city, callback) {
+        const url = `${ WEATHER_DETAILS_ENDPOINT }${ city }`;
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                callback(JSON.parse(xhr.responseText));
+            }
+        }
+        xhr.open('GET', url, true);
+        xhr.send();
+    },
+    render(data) {        
+        console.log(data);
+        data.list.forEach(element => {
+            console.log(new Date(element.dt * 1000));
+        });
+
+        let daySwitcher = document.createElement('ul');
+        let windForecast = document.createElement('section');
+        let dayWind;
+        let dailyForecast = document.createElement('section');
+        let dayWeather;
+
+        for (let i = 0; i < data.list.length; i += 2) {
+            console.log(new Date(data.list[i].dt * 1000));
+            let newNode = document.createElement(`li`);
+            let isNewDay = true;
+            newNode.classList.add('menu-link');
+            newNode.innerHTML = `${new Date(data.list[i].dt * 1000).toLocaleString('ru-RU', {weekday: "short"})}`;
+            if (!newNode.isEqualNode(daySwitcher.lastChild))  {
+                isNewDay = true;
+                daySwitcher.appendChild(newNode);
+            } else {
+                isNewDay = false;
+            }
+
+            if (isNewDay) {
+                dayWind = document.createElement(`section`);
+                dayWind.classList.add('day-wind');
+                dayWeather = document.createElement('section');
+                dayWeather.classList.add('day-weather');
+                //TODO: replace first letter of weekday by capital letter
+                let dayNameNode = document.createElement(`p`);
+                dayNameNode.classList.add(`day-name`);
+                dayNameNode.innerHTML = `${new Date(data.list[i].dt * 1000).toLocaleString('ru-RU', {weekday: "short"})}, 
+                                    ${new Date(data.list[i].dt * 1000).toLocaleString('ru-RU', {day: 'numeric', month: 'long'})}`;
+                dayWeather.appendChild(dayNameNode);
+            } else {
+                dayWind = windForecast.lastChild;
+                dayWeather = dailyForecast.lastChild;
+            }
+            let windItemNode = document.createElement(`span`);
+            windItemNode.classList.add(`item`);
+            windItemNode.innerHTML = `<p><span>${Math.round(data.list[i].wind.speed)}</span></p>`;
+            dayWind.appendChild(windItemNode);
+            windForecast.appendChild(dayWind);
+            dayWeather.appendChild()
+        }
+        daySwitcher.firstChild.classList.add('selected');
+        document.getElementById('day-switcher').appendChild(daySwitcher);
+        document.getElementById('weather-container').insertBefore(windForecast, document.getElementById('precepitation-forecast-label'));
+        document.getElementById('five-days-container').insertBefore(dailyForecast, document.getElementById('next-day-container'));
+        
+        // DOM manipulation 
+    },
+};
+
+page.init();
