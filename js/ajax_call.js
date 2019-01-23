844dce0af8d0a08f1a985d6f753a0664d94d970d
nastya <nast.ovc@yandex.ru>
2019-01-23 08:10:03 +0400

    air pollution retrieving added

--- js/ajax_call.js
+++ js/ajax_call.js
@@ -1,17 +1,18 @@
 const APP_ID = 'f9a8867923bcc86f488c9142432dd2de';
 const WEATHER_DETAILS_ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&APPID=${ APP_ID }&q=`;
-//const AIR_POLLUTION_ENDPOINT = `http://api.openweathermap.org/pollution/v1/co/${ coords }/current.json?appid=${ APP_ID}`;
+const AIR_POLLUTION_ENDPOINT = [`http://api.openweathermap.org/pollution/v1/co/`, `/current.json?appid=${ APP_ID}`];
 const defaultCity = 'izhevsk';
-let coords;
+let defaultCoords = `56,53`;
 const page = {
     init: function() {
-        this.getWeatherDetails(defaultCity, this.render);
-
+        this.getWeatherDetails(defaultCity, this.renderWeatherDetails);
+        this.getAirPollution(defaultCoords, this.renderAirPollution);
         const searchField = document.getElementById('search-field');
         searchField.addEventListener('change', (event) => {
+            
             const city = event.target.value;
-            console.log(this);
-            this.getWeatherDetails(city, this.render);
+            this.getWeatherDetails(city, this.renderWeatherDetails);
+            
         });
     },
     getWeatherDetails(city, callback) {
@@ -25,7 +26,8 @@ const page = {
         xhr.open('GET', url, true);
         xhr.send();
     },
-    render(data) { 
+    renderWeatherDetails(data) { 
+        coords = `${ Math.floor(data.coord.lat) },${ Math.floor(data.coord.lon) }`;
         document.getElementById('chosen-location').innerHTML = `${data.name}, ${data.sys.country}`;
         document.getElementById('today-weekday').innerHTML = new Date(data.dt * 1000).toLocaleString('ru-RU', {weekday: "long"});
         document.getElementById('weather-description').innerHTML = data.weather[0].description;
@@ -46,6 +48,28 @@ const page = {
         }
         // DOM manipulation 
     },
+    getAirPollution(coords, callback) {
+        const url = `${ AIR_POLLUTION_ENDPOINT[0] }${ coords }${ AIR_POLLUTION_ENDPOINT[1] }`;
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                callback(JSON.parse(xhr.responseText));
+            }
+        }
+        xhr.open('GET', url, true);
+        xhr.send();
+    },
+
+    renderAirPollution(data) {
+        let element = data.data.find((element) => {
+            if (Math.floor(element.pressure) === 215) {
+                return element;    
+            }
+        });
+        document.getElementById('air-pollution').innerHTML = `Загрязнение воздуха: ${element.value}`;
+        console.log(element);
+        console.log(data);
+    }
 };
 
 page.init();
