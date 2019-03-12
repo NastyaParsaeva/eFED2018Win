8299da56b72956bfcb0dbcff23ca330ae94275d4
nastya <nast.ovc@yandex.ru>
2019-03-12 20:37:30 +0400

    slider for five days page fixed

--- js/five-days-page/five-days-slider.js
+++ js/five-days-page/five-days-slider.js
@@ -1,49 +1,52 @@
 
-function FiveDaysSlider() {
-    this.next = document.getElementById('next-day');
-    this.prev = document.getElementById('prev-day');
-    this.initializeArrowEventListeners();
+class FiveDaysSlider {
+    constructor() {
+        this.next = document.getElementById('next-day');
+        this.prev = document.getElementById('prev-day');
+        this.initializeArrowEventListeners();
+    }
+    nextSlide() {
+        this.changeSlide((this.current + 1) % this.slidesArrayContainer.daySlides.length);
+    }
+    previousSlide() {
+        (this.current !== 0) ? this.changeSlide(this.current - 1) : this.changeSlide(this.slidesArrayContainer.daySlides.length - 1);
+    }
+    changeSlide(index) {
+        removeDomClassName(this.slideNav, this.current, 'selected');
+        for (let slidesType in this.slidesArrayContainer) {
+            removeDomClassName(this.slidesArrayContainer[slidesType], this.current, 'shown');
+        }
+        this.current = index;
+        for (let slidesType in this.slidesArrayContainer) {
+            addDomClassName(this.slidesArrayContainer[slidesType], this.current, 'shown');
+        }
+        addDomClassName(this.slideNav, this.current, 'selected');
+    }
+    initializeSliderElements() {
+        this.current = 0;
+        this.slidesArrayContainer = {
+            daySlides: getElementsByQuery('.day-weather'),
+            windSlides: getElementsByQuery('.day-wind'),
+            precSlides: getElementsByQuery('.day-precepitation')
+        };
+        this.slideNav = getElementsByQuery('.day-switcher .menu-link');
+        this.initializeSlideNavListeners();
+    }
+    initializeArrowEventListeners() {
+        this.next.addEventListener('click', () => {
+            this.nextSlide();
+        });
+        this.prev.addEventListener('click', () => {
+            this.previousSlide();
+        });
+    }
+    initializeSlideNavListeners() {
+        for (let i = 0; i < this.slideNav.length; i++) {
+            this.slideNav[i].addEventListener('click', () => {
+                this.changeSlide(i);
+            });
+        }
+    }
 }
 
-FiveDaysSlider.prototype = Object.create(Slider.prototype);
 
-FiveDaysSlider.prototype.nextSlide = function() {
-    this.changeSlide((this.current + 1) % this.slidesArrayContainer.daySlides.length);
-},
-
-FiveDaysSlider.prototype.previousSlide = function() {
-    (this.current !== 0) ? this.changeSlide(this.current - 1) : this.changeSlide(this.slidesArrayContainer.daySlides.length - 1);
-},
-
-FiveDaysSlider.prototype.changeSlide = function(index) {
-    for (slidesType in this.slidesArrayContainer) {
-        removeDomClassName(this.slidesArrayContainer[slidesType], this.current, 'shown');
-    }
-    removeDomClassName(this.slideNav, this.current, 'selected');
-    this.current = index;
-
-    for (slidesType in this.slidesArrayContainer) {
-        addDomClassName(this.slidesArrayContainer[slidesType], this.current, 'shown');
-    }
-    addDomClassName(this.slideNav, this.current, 'selected');
-};
-
-FiveDaysSlider.prototype.initializeSliderElements = function() {
-    this.current = 0;
-    this.slidesArrayContainer = {
-        daySlides: getElementsByQuery('.day-weather'),
-        windSlides: getElementsByQuery('.day-wind'),
-        precSlides: getElementsByQuery('.day-precepitation')
-    };
-    this.slideNav = getElementsByQuery('.day-switcher .menu-link');
-    this.initializeSlideNavListeners();
-};
-
-FiveDaysSlider.prototype.initializeArrowEventListeners = function() {
-    this.next.addEventListener('click', () => {
-        this.nextSlide();
-    });
-    this.prev.addEventListener('click', () => {
-        this.previousSlide();
-    });
-};
